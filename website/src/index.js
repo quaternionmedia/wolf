8f37df2871a5fe85edd3df69ba0e510627fcab59
Harpo <harpo@harpo.me>
2020-08-20 13:06:50 -0700

    boilerplate mithril code!

--- website/src/index.js
+++ website/src/index.js
@@ -1,1 +1,15 @@
 console.log('wolf!')
+
+import m from 'mithril'
+
+export function Home() {
+  return {
+    view: vnode => {
+      return m('#home', {}, 'wolf!')
+    }
+  }
+}
+
+m.route(document.body, "/", {
+  "/": Home,
+})
