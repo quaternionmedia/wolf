36256c0ce0ca9c3d2093621b1e1276eea06f37d9
Harpo <harpo@harpo.me>
2020-09-01 18:08:05 -0700

    added drum map!

--- midistream.py
+++ midistream.py
@@ -7,9 +7,11 @@ holophonor = jack.open_output('holophonor', client_name='holoOutput')
 automation = jack.open_output('automation', client_name='otto')
 drums = jack.open_output('drums', client_name='animal')
 
-launchpad_holophonor = [0,1,2,3,16,17,18,19,32,33,34,35,48,49,50,51,64,65,66,67,80,81,82,83,96,97,98,99,112,113,114,115]
+launchpad_holophonor = [0,1,2,3,16,17,18,19,32,33,34,35,48,49,50,51,64,65,66,67,80,81,82,83,96,97,98,99,108,112,113,114,115]
 launchpad_automation = [4,5,6,7,20,21,22,23,36,37,38,39,52,53,54,55]
 launchpad_drums = [68,69,70,71,84,85,86,87,100,101,102,103,116,117,118,119]
+
+drum_map = [32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47]
 
 active = set()
 
@@ -41,6 +43,7 @@ async def print_messages():
                 m = Message('control_change', channel=15, control=message.note, value=velocity)
                 automation.send(m)
             elif message.note in launchpad_drums:
+                message.note = drum_map[launchpad_drums.index(message.note)]
                 drums.send(message)
 
 if __name__ == '__main__':
