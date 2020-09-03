6142b04a234b5d12e3781b63eab7edda9dec4dbf
Harpo <harpo@harpo.me>
2020-09-02 20:12:35 -0700

    updated rack.json structure

--- soup.py
+++ soup.py
@@ -6,16 +6,15 @@ def parseXml(f):
 
 def rackControl(filename, control = 0):
     soup = parseXml(filename)
-    cc = []
+    rack = {}
     for i, plugin in enumerate(soup.findAll('plugin')):
         pluginName = plugin['instance-name']
-        params = []
+        params = {}
         for j, param in enumerate(plugin.preset.findAll('param')):
-            params.append({
-                'name': param['name'],
+            params[param['name']] = {
                 'value': param['value'],
                 'control': control,
-            })
+            }
             auto = soup.new_tag('automation')
             auto['key'] = f'automation_v1_{control}_to_{param["name"]}'
             auto['value'] = '0 1'
@@ -24,19 +23,19 @@ def rackControl(filename, control = 0):
                 control += 129
             else:
                 control += 1
-        cc.append({ pluginName: params })
-    return soup, cc, control
+        rack[pluginName] = params
+    return soup, rack, control
 
 if __name__ == '__main__':
     from os.path import join
     from json import dumps
     racks = ['calf_vocal', 'calf_submix', 'calf_synth', 'calf_postfx', 'calf_jacktrip']
     control = 0
-    plugins = []
-    for rack in racks:
-        soup, cc, control = rackControl(join('../holophonor', rack), control=control)
-        with open(join('../holophonor', rack + '_auto.xml'), 'w') as f:
+    plugins = {}
+    for rackName in racks:
+        soup, rack, control = rackControl(join('../holophonor', rackName), control=control)
+        with open(join('../holophonor', rackName + '_auto.xml'), 'w') as f:
             f.write(soup.prettify())
-        plugins.append({rack: cc})
+        plugins[rackName] = rack
     with open('racks.json', 'w') as f:
-        f.write(dumps({'racks': plugins}))
+        f.write(dumps(plugins))
