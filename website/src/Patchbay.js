e0e773bf5e97125a080b43bf56f711b6952ac518
Harpo <harpo@harpo.me>
2020-09-21 11:27:50 -0700

    default grid layout

--- website/src/Patchbay.js
+++ website/src/Patchbay.js
@@ -5,49 +5,103 @@ import edgehandles from 'cytoscape-edgehandles'
 cytoscape.use( edgehandles )
 
 export function Patchbay() {
-  let cy
+  let cy, eh
   return {
     oncreate: vnode => {
       cy = cytoscape({
         container: vnode.dom,
         elements: [ // list of graph elements to start with
-    { // node a
-      data: { id: 'a' }
-    },
-    { // node b
-      data: { id: 'b' }
-    },
-    { // edge ab
-      data: { id: 'ab', source: 'a', target: 'b' }
-    }
-  ],
+        { // node a
+          data: { id: 'a' }
+        },
+        { // node b
+          data: { id: 'b' }
+        },
+        { // edge ab
+          data: { id: 'ab', source: 'a', target: 'b' }
+        }
+      ],
+      layout: {
+               name: 'grid',
+               padding: 0,
+             },
+      style: [
+           {
+             selector: 'node[name]',
+             style: {
+               'content': 'data(name)'
+             }
+           },
 
-  style: [ // the stylesheet for the graph
-    {
-      selector: 'node',
-      style: {
-        'background-color': '#666',
-        'label': 'data(id)'
-      }
-    },
+           {
+             selector: 'edge',
+             style: {
+               'curve-style': 'bezier',
+               'target-arrow-shape': 'triangle'
+             }
+           },
 
-    {
-      selector: 'edge',
-      style: {
-        'width': 3,
-        'line-color': '#ccc',
-        'target-arrow-color': '#ccc',
-        'target-arrow-shape': 'triangle',
-        'curve-style': 'bezier'
-      }
-    }
-  ],
+           // some style for the extension
+           {
+             selector: '.eh-handle',
+             style: {
+               'background-color': 'red',
+               'width': 12,
+               'height': 12,
+               'shape': 'ellipse',
+               'overlay-opacity': 0,
+               'border-width': 12, // makes the handle easier to hit
+               'border-opacity': 0
+             }
+           },
 
-  layout: {
-    name: 'grid',
-    rows: 1
-  }
+           {
+             selector: '.eh-hover',
+             style: {
+               'background-color': 'red'
+             }
+           },
+
+           {
+             selector: '.eh-source',
+             style: {
+               'border-width': 2,
+               'border-color': 'red'
+             }
+           },
+
+           {
+             selector: '.eh-target',
+             style: {
+               'border-width': 2,
+               'border-color': 'red'
+             }
+           },
+
+           {
+             selector: '.eh-preview, .eh-ghost-edge',
+             style: {
+               'background-color': 'red',
+               'line-color': 'red',
+               'target-arrow-color': 'red',
+               'source-arrow-color': 'red'
+             }
+           },
+           {
+             selector: '.eh-ghost-edge.eh-preview-active',
+             style: {
+               'opacity': 0
+             }
+           }
+         ],
       })
+
+      eh = cy.edgehandles({
+        noEdgeEventsInDraw: true,
+        snap: true
+      })
+      // let grid = cy.layout({name: 'grid'})
+      // grid.run()
     },
     view: vnode => {
       return m('.patchbay#patchbay', {}, )
