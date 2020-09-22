1385c2c381218b9266132c96fb07ded98fbd10e8
Harpo <harpo@harpo.me>
2020-09-22 10:14:19 -0700

    added parent info to networkx graph!!

--- wolf.py
+++ wolf.py
@@ -1,8 +1,9 @@
 from mido import Backend, Message
 from fastapi import FastAPI, WebSocket
 from starlette.staticfiles import StaticFiles
-from networkx import Graph
-from networkx.readwrite.json_graph import cytoscape_data
+from networkx import Graph, set_node_attributes
+# from networkx.readwrite.json_graph import cytoscape_data
+from cyto import cytoscape_data
 from json import loads
 from jack import Client
 
@@ -17,6 +18,12 @@ async def sendCC(channel: int = 0, control: int = 0, value: int = 0):
     message = Message('control_change', channel=channel, control=control, value=value)
     outport.send(message)
 
+@app.get('/note')
+async def sendCC(channel: int = 0, note: int = 0, velocity: int = 0):
+    message = Message('note_on', channel=channel, note=note, velocity=velocity)
+    outport.send(message)
+
+
 @app.get('/channels')
 async def getChannels():
     with open('racks.json') as f:
@@ -27,12 +34,18 @@ async def getConnections():
     graph = Graph()
     ports = [ p.name for p in client.get_ports() ]
     print(ports)
+    clients = set()
     graph.add_nodes_from(ports)
+    set_node_attributes(graph, { p:{'parent': p.split(':')[0]} for p in ports})
     for p in ports:
+
+        clientName = p.split(':')[0]
+        clients.add(clientName)
         c = [ (p, c.name) for c in client.get_all_connections(p) ]
         if c:
             print(f'connection from {p} to {c}')
             graph.add_edges_from(c)
+    graph.add_nodes_from(clients)
     print('graph', graph.nodes, graph.edges)
     return cytoscape_data(graph)
 
