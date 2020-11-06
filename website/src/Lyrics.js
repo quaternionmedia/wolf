47a2b42a5092a1efc72749925663185aa5f2de91
Harpo <harpo@harpo.me>
2020-11-06 13:09:05 -0800

    working fuse filter!!!

--- website/src/Lyrics.js
+++ website/src/Lyrics.js
@@ -1,17 +1,34 @@
 import m from 'mithril'
+import Fuse from 'fuse.js'
 
 export function Lyrics() {
   let setlist = []
+  let fuse = new Fuse(setlist, {
+    threshold: .4,
+  })
+  let filter = ''
   return {
     oninit: (vnode) => {
       m.request('/setlist').then(res => {
         setlist = res
+        fuse.setCollection(res)
       })
     },
     view: (vnode) => {
-      return m('ul.setlist', {}, setlist.map(song => {
-        return m('li', {}, song)
-      }))
+      return [
+        m('input.search', {oninput: (v) => {
+          console.log('searching', v.target.value)
+          filter = v.target.value
+          m.redraw()
+        }}, []),
+        m('ul.setlist', {}, [
+          filter ? fuse.search(filter).map(song => {
+          return m('li', {}, song.item)
+        }) : setlist.map(song => {
+          return m('li', {}, song)
+        })
+        ])
+      ]
     }
   }
 }
