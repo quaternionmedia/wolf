a5bce71672c82449a8ea174273a0d9c89e4fdd64
Harpo <harpo@harpo.me>
2020-11-07 11:45:13 -0800

    working realbook file splitter!!

---
+++ realbook.py
@@ -0,0 +1,25 @@
+from pdfrw import PdfReader, PdfWriter
+
+def openCsv(path):
+    import csv
+    results = []
+    with open(path) as csvFile:
+        reader = csv.reader(csvFile)
+        return list(reader)
+        
+def parseReal(book, index, offset=0):
+    index = openCsv(index)
+    book = PdfReader(book)
+    for i in range(len(index)):
+        song = index[i]
+        start_page = int(song[1]) + offset
+        if i < len(index) - 1:
+            end_page = int(index[i+1][1]) + offset
+        else:
+            end_page = int(index[i][1]) + 1 + offset
+        if start_page == end_page: end_page += 1
+        print(song, start_page, end_page)
+        writer = PdfWriter()
+        for p in range(start_page, end_page):
+            writer.addpage(book.pages[p])
+        writer.write(f'static/pdf/{song[0].title()}.pdf')
