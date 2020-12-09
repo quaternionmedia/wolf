73b19597d77a51b8bf1c8f14e273a5b2208a276b
Harpo <harpo@harpo.me>
2020-12-08 17:13:21 -0800

    working launchpad loopback!!

--- midihub.py
+++ midihub.py
@@ -1,23 +1,32 @@
 import rtmidi
 from time import time, sleep
+from collections import deque
+from rtmidi.midiutil import open_midioutput, open_midiinput
 
-midiout = rtmidi.MidiOut()
 midiin = rtmidi.MidiIn()
+midiout = rtmidi.MidiOut()
+holoOut, p = open_midioutput('FreeWheeling:FreeWheeling IN 1 129:0', client_name='holoOut')
+holoIn, p = open_midiinput('FreeWheeling:FreeWheeling OUT 1 129:1', client_name='holoIn')
+launchOut, p = open_midioutput('Launchpad X:Launchpad X MIDI 2 16:1', client_name='launchOut')
+launchIn, p = open_midiinput('Launchpad X:Launchpad X MIDI 2 16:1', client_name='launchIn')
 
 out_ports = midiout.get_ports()
+print('out ports:')
+print(out_ports)
 in_ports = midiin.get_ports()
+print('in ports:')
+print(in_ports)
 
-# if out_ports:
-#     midiout.open_port(0)
-# else:
-midiout.open_virtual_port("hub")
-
-if in_ports:
-    midiin.open_port(2)
-else:
-    midiin.open_virtual_port("My virtual output")
-
-# with midiin:
+launchpad_loops = {}
+n = 81
+i = 0
+for y in range(4):
+    for x in range(8):
+        launchpad_loops[n + x] = i
+        i += 1
+    n -= 10
+print('loop map:')
+print(launchpad_loops)
 
 class MidiHandler:
     def __init__(self):
@@ -29,13 +38,29 @@ class MidiHandler:
         self.wallclock += deltatime
         print(self.wallclock, message)
 
-midiin.set_callback(MidiHandler())
+class HoloController:
+    def __init__(self):
+        self.loops = deque([], 32)
+        self.map = deque([])
+        self.scenes = deque([], 8)
+        pass
+    def __call__(self, event, data=None):
+        message, deltatime = event
+        print('note on', message)
+        if message[0] == 144:
+            if message[1] > 0:
+                launchOut.send_message(message)
+            else:
+                # note off
+                pass
+launchIn.set_callback(HoloController())
+
 try:
     while True:
         sleep(.01)
 except KeyboardInterrupt:
     print('')
 finally:
-    print("Exit.")
+    print("hub out!")
     midiin.close_port()
     del midiin
